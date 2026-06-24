a4dc296b537346711520d115f02d2c9c2c463b39
Tejaswini <chennamsettitejaswini5@gmail.com>
2026-06-24 11:15:53 +0530

    updated backend URL for deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://task-manager-capstone.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
